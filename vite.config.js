70982e9bcdd2720621584aec330ba06d26310e95
Yared Molla <84707946+yared-dallol@users.noreply.github.com>
2024-11-19 22:56:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/bethelge/Netflix-Clone-2024/',
+  base: 'https://github.com/yared-dallol/test',
 })
